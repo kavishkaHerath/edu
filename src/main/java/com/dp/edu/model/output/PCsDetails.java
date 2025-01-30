0e61e62183d8566e8dba21d30ddf361bb3d4cb71
kavishkaHerath <hmravindu@gmail.com>
2025-01-30 08:53:36 +0530

    add PCsDetails model

---
+++ src/main/java/com/dp/edu/model/output/PCsDetails.java
@@ -0,0 +1,10 @@
+package com.dp.edu.model.output;
+
+import lombok.Data;
+
+@Data
+public class PCsDetails {
+    private String pcsCode;
+    private String registerDate;
+    private String centerCode;
+}
