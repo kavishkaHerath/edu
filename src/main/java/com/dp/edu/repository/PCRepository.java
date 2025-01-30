68108606ace80972caff95c92ed285bd9d28bc86
kavishkaHerath <hmravindu@gmail.com>
2025-01-30 09:38:34 +0530

    add method in PC repository for get center's pcs

--- src/main/java/com/dp/edu/repository/PCRepository.java
+++ src/main/java/com/dp/edu/repository/PCRepository.java
@@ -1,7 +1,12 @@
 package com.dp.edu.repository;
 
+import com.dp.edu.model.output.PCsDetails;
+import com.dp.edu.model.tables.Center;
 import com.dp.edu.model.tables.PC;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface PCRepository extends JpaRepository<PC, String> {
+    List<PC> findByCenter(Center center);
 }
