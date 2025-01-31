c29a416079b4446d9b0854d729a07725eb56669c
kavishkaHerath <hmravindu@gmail.com>
2025-01-31 14:26:01 +0530

    add find email method

--- src/main/java/com/dp/edu/repository/UserRepository.java
+++ src/main/java/com/dp/edu/repository/UserRepository.java
@@ -4,4 +4,5 @@ import com.dp.edu.model.tables.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, String> {
+    boolean existsByEmail(String email);
 }
