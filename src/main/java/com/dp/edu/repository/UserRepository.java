6bc12ab8856d42e2c42f18b0c9b638fefe0e1575
kavishkaHerath <hmravindu@gmail.com>
2025-02-01 15:00:01 +0530

    add existsByPasswordAndEmail method

--- src/main/java/com/dp/edu/repository/UserRepository.java
+++ src/main/java/com/dp/edu/repository/UserRepository.java
@@ -2,7 +2,12 @@ package com.dp.edu.repository;
 
 import com.dp.edu.model.tables.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface UserRepository extends JpaRepository<User, String> {
     boolean existsByEmail(String email);
+
+    boolean existsByPasswordAndEmail(String password, String email);
+
+  //  User findByEmail(String email);
 }
