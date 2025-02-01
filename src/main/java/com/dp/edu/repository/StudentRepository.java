1fd0802c444472f8cbbc543806a7512b41e93885
kavishkaHerath <hmravindu@gmail.com>
2025-02-01 20:08:27 +0530

    add Student Repository

---
+++ src/main/java/com/dp/edu/repository/StudentRepository.java
@@ -0,0 +1,7 @@
+package com.dp.edu.repository;
+
+import com.dp.edu.model.tables.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StudentRepository extends JpaRepository<Student, String> {
+}
