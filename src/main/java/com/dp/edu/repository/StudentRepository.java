4f68a53ab01706ce9e99955af53d1bf0231e6362
kavishkaHerath <hmravindu@gmail.com>
2025-02-01 22:21:00 +0530

    add student count method

--- src/main/java/com/dp/edu/repository/StudentRepository.java
+++ src/main/java/com/dp/edu/repository/StudentRepository.java
@@ -2,7 +2,11 @@ package com.dp.edu.repository;
 
 import com.dp.edu.model.tables.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface StudentRepository extends JpaRepository<Student, String> {
     boolean existsByEmail(String email);
+
+    @Query("SELECT COUNT(s) FROM Student s WHERE s.center.centerCode = ?1")
+    int countByCenterCode(String centerCode);
 }
