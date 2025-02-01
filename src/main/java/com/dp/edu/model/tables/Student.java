d14b21e52c303b4fe28ea37a999c6fd39d8a8417
kavishkaHerath <hmravindu@gmail.com>
2025-02-01 20:07:02 +0530

    add student model

---
+++ src/main/java/com/dp/edu/model/tables/Student.java
@@ -0,0 +1,35 @@
+package com.dp.edu.model.tables;
+
+import jakarta.persistence.*;
+
+import javax.xml.crypto.Data;
+import java.util.Date;
+
+@Entity
+@Table(name = "TBL_STUDENTS")
+public class Student {
+    @Id
+    @Column(name = "STR_STUDENT_CODE", nullable = false, length = 12)
+    private String studentCode;
+    @Column(name = "STR_STUDENT_NAME", nullable = false)
+    private String name;
+    @Column(name = "STR_EMAIL", nullable = false, unique = true)
+    private String email;
+
+    @ManyToOne
+    @JoinColumn(name = "STR_CENTER_CODE", referencedColumnName = "STR_CENTER_CODE", nullable = false)
+    private Center center;
+
+    @Column(name = "STR_PHONE_NUMBER", length = 10)
+    private String phoneNumber;
+    @Column(name = "STR_PARENT_PHONE_NUMBER", length = 10)
+    private String parentPhoneNumber;
+    @Column(name = "STR_ADDRESS")
+    private String address;
+    @Column(name = "DATE_ADD_DATE", nullable = false)
+    @Temporal(TemporalType.DATE)
+    private Date addDate;
+
+
+
+}
