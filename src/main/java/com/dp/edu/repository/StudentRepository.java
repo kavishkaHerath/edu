42b23d3d433c220deae5558e2175c92e586b3c8a
kavishkaHerath <hmravindu@gmail.com>
2025-02-01 22:17:44 +0530

    crete existsByEmail method

--- src/main/java/com/dp/edu/repository/StudentRepository.java
+++ src/main/java/com/dp/edu/repository/StudentRepository.java
@@ -4,4 +4,5 @@ import com.dp.edu.model.tables.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface StudentRepository extends JpaRepository<Student, String> {
+    boolean existsByEmail(String email);
 }
