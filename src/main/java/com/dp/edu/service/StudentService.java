1d87586468c9cf0e86c17a8dae639bdd7ef5f0fe
kavishkaHerath <hmravindu@gmail.com>
2025-02-01 22:19:09 +0530

    crete student

---
+++ src/main/java/com/dp/edu/service/StudentService.java
@@ -0,0 +1,43 @@
+package com.dp.edu.service;
+
+import com.dp.edu.model.tables.Student;
+import com.dp.edu.repository.StudentRepository;
+import com.dp.edu.response.ResponseMessage;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+@Service
+public class StudentService {
+    @Autowired
+    private final StudentRepository studentRepository;
+
+    public StudentService(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+
+    public ResponseEntity<ResponseMessage> createStudent(Student student){
+        try{
+                if(studentRepository.existsByEmail(student.getEmail())){
+                    return ResponseEntity.ok().body(new ResponseMessage(
+                            "-1",
+                            "STUDENT ALREADY INSERT",
+                            ""
+                    ));
+                }
+                var center = student.getCenter().getCenterCode();
+            return ResponseEntity.ok().body(new ResponseMessage(
+                    "0",
+                    "STUDENT ALREADY INSERT",
+                    ""
+            ));
+        }
+        catch (Exception e){
+            return ResponseEntity.ok().body(new ResponseMessage(
+                    "-1",
+                    "Failed to insert student: " + e.getMessage(),
+                    ""
+            ));
+        }
+    }
+}
