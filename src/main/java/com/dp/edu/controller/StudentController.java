16227154c5c5717599a33eb7d0c6c329002baa32
kavishkaHerath <hmravindu@gmail.com>
2025-02-02 10:18:51 +0530

    Add Student Controller and createStudents post request

---
+++ src/main/java/com/dp/edu/controller/StudentController.java
@@ -0,0 +1,28 @@
+package com.dp.edu.controller;
+
+import com.dp.edu.model.request.StudentDTO;
+import com.dp.edu.model.tables.Student;
+import com.dp.edu.response.ResponseMessage;
+import com.dp.edu.service.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("students")
+@CrossOrigin("*")
+public class StudentController {
+    @Autowired
+    private final StudentService studentService;
+
+    public StudentController(StudentService studentService) {
+        this.studentService = studentService;
+    }
+
+
+    @PostMapping(value = "/insert/student")
+    @ResponseBody
+    public ResponseEntity<ResponseMessage> createStudents(@RequestBody StudentDTO student){
+        return studentService.createStudent(student);
+    }
+}
