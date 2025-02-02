2bb4266b3011a27ba2c8212930d0e3e67e47c0cb
kavishkaHerath <hmravindu@gmail.com>
2025-02-02 10:17:58 +0530

    change student model and add studentDTO model

---
+++ src/main/java/com/dp/edu/model/request/StudentDTO.java
@@ -0,0 +1,83 @@
+package com.dp.edu.model.request;
+
+import lombok.Data;
+
+@Data
+public class StudentDTO {
+    private String studentCode;
+    private String studentName;
+    private String email;
+    private String centerCode;
+    private String phoneNumber;
+    private String parentPhoneNumber;
+    private String address;
+
+    public StudentDTO() {
+    }
+
+    public StudentDTO(String studentCode, String studentName, String email, String centerCode, String phoneNumber, String parentPhoneNumber, String address) {
+        this.studentCode = studentCode;
+        this.studentName = studentName;
+        this.email = email;
+        this.centerCode = centerCode;
+        this.phoneNumber = phoneNumber;
+        this.parentPhoneNumber = parentPhoneNumber;
+        this.address = address;
+    }
+
+    public String getStudentCode() {
+        return studentCode;
+    }
+
+    public void setStudentCode(String studentCode) {
+        this.studentCode = studentCode;
+    }
+
+    public String getStudentName() {
+        return studentName;
+    }
+
+    public void setStudentName(String studentName) {
+        this.studentName = studentName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getCenterCode() {
+        return centerCode;
+    }
+
+    public void setCenterCode(String centerCode) {
+        this.centerCode = centerCode;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getParentPhoneNumber() {
+        return parentPhoneNumber;
+    }
+
+    public void setParentPhoneNumber(String parentPhoneNumber) {
+        this.parentPhoneNumber = parentPhoneNumber;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
--- src/main/java/com/dp/edu/model/tables/Student.java
+++ src/main/java/com/dp/edu/model/tables/Student.java
@@ -31,8 +31,81 @@ public class Student {
     @Temporal(TemporalType.DATE)
     private Date addDate;
 
+    public Student() {
+    }
 
+    public Student(String studentCode, String name, String email, Center center, String phoneNumber, String parentPhoneNumber, String address, Date addDate) {
+        this.studentCode = studentCode;
+        this.name = name;
+        this.email = email;
+        this.center = center;
+        this.phoneNumber = phoneNumber;
+        this.parentPhoneNumber = parentPhoneNumber;
+        this.address = address;
+        this.addDate = addDate;
+    }
 
+    public String getStudentCode() {
+        return studentCode;
+    }
 
+    public void setStudentCode(String studentCode) {
+        this.studentCode = studentCode;
+    }
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Center getCenter() {
+        return center;
+    }
+
+    public void setCenter(Center center) {
+        this.center = center;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getParentPhoneNumber() {
+        return parentPhoneNumber;
+    }
+
+    public void setParentPhoneNumber(String parentPhoneNumber) {
+        this.parentPhoneNumber = parentPhoneNumber;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public Date getAddDate() {
+        return addDate;
+    }
+
+    public void setAddDate(Date addDate) {
+        this.addDate = addDate;
+    }
 }
